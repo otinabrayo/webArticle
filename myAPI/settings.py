deb61b9c69738a5e8046d4f492e6186f08f7ec77
Brian Otina <brianotina20@gmail.com>
2024-10-04 18:35:57 +0300

    ...

--- myAPI/settings.py
+++ myAPI/settings.py
@@ -4,9 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_b((5(6hc!gnke^t(vl!d9@k--fygvyj_il!d!zt87&3_m-7vs'    # noqa
 

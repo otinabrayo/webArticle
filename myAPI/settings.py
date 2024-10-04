c857b475e35bd52bc24283c77495a84b8bfe8417
Brian Otina <brianotina20@gmail.com>
2024-10-04 18:36:30 +0300

    ..

--- myAPI/settings.py
+++ myAPI/settings.py
@@ -117,9 +117,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
